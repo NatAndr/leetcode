b4260174e67377478cc9e716d78d1d48ee947ce8
NatAndr <natalia.andrianova@gmail.com>
2020-04-19 21:09:41 +0300

    improve MoveZeroes

--- src/array/MoveZeroes.java
+++ src/array/MoveZeroes.java
@@ -1,29 +1,32 @@
 package array;
 
+import java.util.Arrays;
+
 public class MoveZeroes {
     public void moveZeroes(int[] nums) {
-        if (nums.length < 2) return;
+        int n = nums.length;
+        if (n < 2) return;
 
         int k = 0;
         int i = 0;
 
-        while (i < nums.length - 1 && k < nums.length) {
-            if (nums[i] == 0) {
-                System.arraycopy(nums, i + 1, nums, i, nums.length - i - 1);
-                nums[nums.length - 1] = 0;
-            } else {
+        while (i < n && k < n) {
+            if (nums[k] != 0) {
+                nums[i] = nums[k];
+                if (i != k) {
+                    nums[k] = 0;
+                }
                 i++;
             }
             k++;
         }
-
-        System.out.println();
     }
 
     public static void main(String[] args) {
-        int[] arr = new int[]{0, 0, 1, 0, 1, 2, 2, 0, 0, 4};
+        int[] arr = new int[]{0, 0, 1};
 //        int[] arr = new int[]{1};
-//        int[] arr = new int[]{0,1,0,3,12};
+//        int[] arr = new int[]{0, 1, 0, 3, 12};
         new MoveZeroes().moveZeroes(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
